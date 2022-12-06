4ace53656c8dfa19760aa5845192e70cb8bddce8
sorube13 <sorube@gmail.com>
2022-12-06 12:05:08 +0100

    Day 3 & 4

---
+++ 2022/day3.ts
@@ -0,0 +1,38 @@
+import { readFileInput } from '../tools-ts';
+
+let rucksacks: string[] = readFileInput('./inputs/day3.txt');
+const priorities = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+/*let sumPriority = 0;
+
+for (let rucksack of rucksacks) {
+  let compartement1 = {};
+  let nbItems = rucksack.length;
+  for (let i = 0; i < nbItems / 2; i++) {
+    if (!compartement1[rucksack[i]]) {
+      compartement1[rucksack[i]] = 0;
+    }
+    compartement1[rucksack[i]] = compartement1[rucksack[i]] + 1;
+  }
+  for (let i = nbItems / 2; i < nbItems; i++) {
+    if (compartement1[rucksack[i]]) {
+      sumPriority += priorities.indexOf(rucksack[i]) + 1;
+      break;
+    }
+  }
+}
+console.log(sumPriority);*/
+
+let sumBadge = 0;
+let ruckIdx = 0;
+while (ruckIdx <= rucksacks.length - 3) {
+  const sortedRucksacks = rucksacks.slice(ruckIdx, ruckIdx + 3).sort((r1, r2) => r1.length - r2.length);
+  for (let ruck of sortedRucksacks[0]) {
+    if (sortedRucksacks[1].includes(ruck) && sortedRucksacks[2].includes(ruck)) {
+      sumBadge += priorities.indexOf(ruck) + 1;
+      break;
+    }
+  }
+  ruckIdx += 3;
+}
+console.log(sumBadge);
